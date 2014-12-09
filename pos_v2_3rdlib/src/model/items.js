1c8774a23ce6d946eb2900352885578951e5b4ba
ywang <657316992@qq.com>
2014-12-09 18:25:10 +0800

    delete items class

--- pos_v2_3rdlib/src/model/items.js
+++
@@ -1,6 +0,0 @@
-function Items(barcode, name, unit, price,count) {
-  Item.call(this,barcode, name, unit, price);
-  this.count = count;
-}
-Items.prototype = Object.create(item.prototype);
-Items.prototype.constructor = Items;
